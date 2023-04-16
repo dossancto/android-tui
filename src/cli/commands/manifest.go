c64016bf41b505fa6d83b2ae0ec3233fd4403fba
= <=>
2023-04-16 00:23:47 -0300

    Add some test commands

--- src/cli/commands/manifest.go
+++ src/cli/commands/manifest.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/lu-css/android-tui/src/files"
 	"github.com/lu-css/android-tui/src/translate-xml"
+	"strings"
 )
 
 var ManifestCommands = []Command{
@@ -12,6 +13,16 @@ var ManifestCommands = []Command{
 		"permittion",
 		Permissions,
 		"Show all permittions",
+	},
+	{
+		"activity",
+		Activities,
+		"Show all permittions",
+	},
+	{
+		"feature",
+		Features,
+		"Show all features",
 	},
 }
 
@@ -49,6 +60,53 @@ func Permissions(args []string) error {
 	println("Listions permittions in project\n")
 	for _, permittion := range manifest.Permissions {
 		println(permittion.Name)
+	}
+
+	return nil
+}
+
+func Features(args []string) error {
+	data, err := files.ReadManifest()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	manifest, _ := translate_xml.ParseManifest(data)
+
+	s := fmt.Sprintf("%-70s %-10s", "Name", "required")
+
+	fmt.Println(s)
+	fmt.Println("===================================================================================")
+
+	for _, feature := range manifest.Features {
+		s := fmt.Sprintf("%-70s %-10t", feature.Name, feature.Required)
+
+		fmt.Println(s)
+	}
+
+	return nil
+}
+
+func Activities(args []string) error {
+	data, err := files.ReadManifest()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	manifest, _ := translate_xml.ParseManifest(data)
+
+	s := fmt.Sprintf("%1s %-70s", "", "Name")
+
+	fmt.Println(s)
+	fmt.Println("===================================================================================")
+
+	for _, activity := range manifest.Application.Activities {
+		activityName := strings.Replace(activity.Name, manifest.Package+".", "", 1)
+		s := fmt.Sprintf("%1s %-20s", "", activityName)
+
+		fmt.Println(s)
 	}
 
 	return nil
